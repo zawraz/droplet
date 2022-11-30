09fc2d371d41e1fb9501b7d7b5e310de8311152e
Konrad Sawras <zawraz@gmail.com>
2022-11-30 14:14:18 +0100

    Responsiveness issues fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,26 +17,76 @@ function App() {
 			</header>
 
 			<main>
-				<h1 className="title">Usługi CNC - 5 osi</h1>
+				<h1 className="title">Usługi CNC</h1>
+				<h2 className="subtitle">
+					5 osiowe CNC <br></br> Drewno, Laminat, MDF
+				</h2>
 				<img src={background_img} alt="" className="primary-image" />
-				<div className="secondary-images">
+				{/* <div className="secondary-images">
 					<img
-						src="img/little-1.jpg"
+						src={background_img}
 						alt=""
 						className="secondary-image"
 					/>
 					<img
-						src="img/little-2.jpg"
+						src={background_img}
 						alt=""
 						className="secondary-image"
 					/>
-				</div>
-				<p className="description">Opis DROPLETA... [UZUPEŁNIĆ]</p>
+				</div> */}
+				<p className="description">
+					Oferujemy usługi frezowania, wykonywane na 5-osiowym centrum
+					frezującym do drewna.
+					<ul>
+						<li>
+							Wykonujemy zarówno proste zlecenia, takie jak
+							frezowanie frontów, gniazd pod żywicę w blatach
+							drewnianych, jak i skomplikowane geometrie m.in.
+							płaskorzeźby na drewnianych drzwiach, złożone
+							geometrie trójwymiarowe czy choćby formy do
+							laminatów.
+						</li>
+						<li>
+							Paleta możliwych do wykonania elementów jest
+							praktycznie nieograniczona i zależy tylko od
+							pomysłowości klienta.
+						</li>
+						<li>Pole robocze maszyny: 2400 x 1300 x 300 [mm]</li>
+						<li>
+							Materiały obrabiane: drewno, materiały
+							drewnopochodne, tworzywa sztuczne, laminaty i nie
+							tylko!
+						</li>
+					</ul>
+					<span className="span__break">
+						Z uwagi na duże doświadczenie w branży obróbki
+						skrawaniem, oraz realizacji wielu nietypowych zleceń,
+						nie boimy się wyzwań.{" "}
+					</span>
+					<span className="span__break">
+						Możliwa współpraca wysyłkowa!
+					</span>{" "}
+					<span className="span__break">
+						Zapraszamy do przesyłania zapytań ofertowych.
+					</span>
+				</p>
 				<button className="btn">Skontaktuj się z nami</button>
 			</main>
 
 			<footer>
-				<div className="social-links">
+				<div className="contact-details">
+					<span className="contact-details-title">
+						DANE KONTAKTOWE
+					</span>
+					<p>Telefon: +48 691 32 59 88</p>
+					<p>E-mail: biuro@cnc5osi.pl</p>
+					<p>
+						<strong>Droplet Sp. z o.o.</strong>
+					</p>
+					<p>ul.Światowida 2 (hala nr 6-7)</p>
+					<p>46-020 Opole</p>
+				</div>
+				{/* <div className="social-links">
 					<a href="#" className="social-link" aria-label="WhatsApp">
 						<img
 							src="img/snapchat-ghost.svg"
@@ -51,7 +101,7 @@ function App() {
 							className="social-img"
 						/>
 					</a>
-				</div>
+				</div> */}
 			</footer>
 		</div>
 	);
